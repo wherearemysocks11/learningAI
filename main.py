8968f6948b858e3855d79fc2926ac15130fcd888
Zachary <zacwstill@gmail.com>
2024-11-25 18:28:55 +0000

    summary

--- main.py
+++ main.py
@@ -6,20 +6,25 @@
 #
 #     $ conda deactivate
 
-#import sys
+import nnfs
 import numpy as np
-#import matplotlib as mpl
+import matplotlib as mpl
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+class Layer_Dense:
+    def __init__(self, num_inputs, num_neurons):
+        self.weights = 0.01 * np.random.randn(num_inputs, num_neurons)
+        self.biases = np.zeros((1, num_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
 
-inputs = [[1.0, 2.0, 3.0, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
-weights = [[0.2, 0.8, -0.5, 1.0],   
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-biases = [2.0, 3.0, 0.5]
 
-layer_outputs = np.dot(inputs, np.array(weights).T)
+X, y = spiral_data(samples=100, classes=3)
 
-print(layer_outputs)
-
+dense1 = Layer_Dense(2, 3)
+dense1.forward(X)
+print(dense1.output)
--- test.py
+++ test.py
@@ -1,17 +1,33 @@
+# To activate this environment, use
+#
+#     $ conda activate my-env
+#
+# To deactivate an active environment, use
+#
+#     $ conda deactivate
+
+#import sys
 import numpy as np
-#import matplotlib as mpl
+import matplotlib as mpl
+from nnfs.datasets import spiral_data
 
-# a network of 4 inputs, 3 neurons (also the outputs)
-inputs = [1.0, 2.0, 3.0]
-weights = [[0.2, 0.8, -0.5, 1.0],   #each list within the list represents weights for a single neuron
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
 
-inputs = np.array([inputs])
-weights = np.array([weights])
+inputs = [[1.0, 2.0, 3.0, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
 
-bias = [2, 3, 0.5]
+weights1 = [[0.2, 0.8, -0.5, 1.0],   
+            [0.5, -0.91, 0.26, -0.5],
+            [-0.26, -0.27, 0.17, 0.87]]
+biases1 = [2.0, 3.0, 0.5]
 
-print(f"Inputs: {inputs}") 
-print(f"weights: {weights}")
-print(f"bias: {bias}")
+weights2 = [[0.1, -0.14, 0.5],   
+            [-0.5, 0.12, -0.33],
+            [-0.44, 0.73, -0.13]]
+biases2 = [-1.0, 2, -0.5]
+
+layer1_outputs = np.dot(inputs, np.array(weights1).T) + biases1
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+print(layer2_outputs)
+
