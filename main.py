c33e9b19670a5b747a5e55c630ad3fd67ae40de0
Zac <zacwstill@gmail.com>
2025-01-03 21:39:53 +0000

    actually scrap that

--- main.py
+++ main.py
@@ -32,8 +32,26 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)  #clips y_pred length
 
-X,y = spiral_data(samples=100, classes=3)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+X,y = spiral_data(samples=5, classes=3)
 
 dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
@@ -47,4 +65,9 @@ activation1.forward(dense1.output)
 dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 
-print(activation2.output[:5])
+#print(activation2.output)
+
+loss_function = Loss_CategoricalCrossEntropy()
+loss = loss_function.calculate(activation2.output, y)
+
+print(f"Loss: {loss}")
