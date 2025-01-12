16c3ccf7f8fdce4305c2500c90cf4b9e3068c506
Zac <zacwstill@gmail.com>
2025-01-12 13:16:09 +0000

    errrr

---
+++ main.py
@@ -0,0 +1,16 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from micrograd import Value
+
+#
+    
+
+
+a = Value(2.0)
+b = Value(-3.0)
+c = Value(10.)
+
+output = a*b + c
+
+print (output)
